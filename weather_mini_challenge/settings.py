fdb26c5dab36d8065c535c2972154d19e40c2a66
Rafael Cavasani <rafael.cavsani@gmail.com>
2020-02-05 08:39:16 -0300

    Allowed Host changed

--- weather_mini_challenge/settings.py
+++ weather_mini_challenge/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'weather-mini-challenge-new.herokuapp.com'
+]
 
 
 # Application definition
